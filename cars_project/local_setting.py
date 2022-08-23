7ff52dfaa32f18b5ecc53f85726d225b9ea8daf7
NotNanetteKaye <nanettekayedolera@gmail.com>
2022-08-23 17:53:54 -0500

    created local_setting.py

---
+++ cars_project/local_setting.py
@@ -0,0 +1,16 @@
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = 'django-insecure-9&j39xq!ubr8h^$8&+wbv!muvde^w-@(!a@bb^qt(yv)@#!i^d'
+
+# Database
+# https://docs.djangoproject.com/en/4.1/ref/settings/#databases
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'mysql.connector.django',
+        'NAME': 'cars_database',
+        'HOST': 'localhost',
+        'USER': 'root',
+        'PASSWORD': 'AteDarling1!!!'
+    }
+}
