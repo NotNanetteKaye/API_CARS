ebb5896c46fce0f387224ab77c82af457cf2f1a3
NotNanetteKaye <nanettekayedolera@gmail.com>
2022-09-01 17:02:25 -0500

    Created Update Function in cars views.py

--- cars/views.py
+++ cars/views.py
@@ -1,7 +1,12 @@
+from django.shortcuts import get_object_or_404
+from os import stat
+from urllib import response
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from rest_framework import status
 from .serializers import CarSerializer
 from .models import Car
+from cars import serializers
 
 @api_view(['GET', 'POST'])
 def cars_list(request):
@@ -9,12 +14,22 @@ def cars_list(request):
     if request.method == 'GET':
         cars = Car.objects.all()
         serializer = CarSerializer(cars, many=True)
-        return Response(serializer.data, status= 201)
+        return Response(serializer.data, status= status.HTTP_201_CREATED)
 
     elif request.method == 'POST':
         serializer = CarSerializer(data=request.data)
-        if serializer.is_valid() == True:
-            serializer.save()
-            return Response(serializer.data)
-        else:
-            return Response(serializer.errors, status= 400)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+@api_view(['GET', 'PUT'])
+def car_detail(request, pk):
+    car = get_object_or_404(Car, pk=pk)
+    if request.method == 'GET':
+        serializer = CarSerializer(car)
+        return Response(serializer.data)
+    elif request.method == 'PUT':
+        serializer=CarSerializer(car, data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data)
